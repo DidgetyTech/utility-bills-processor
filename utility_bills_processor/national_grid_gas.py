e0681500aa2aaed994a9ce6ff57afda7f9726aa1
DidgetyTech <32182119+DidgetyTech@users.noreply.github.com>
2024-03-23 16:37:27 -0400

    Fix issues when bill includes messages or there's an acceptable failed check
    The regex is modified to prevent data embedded in message callouts from
    matching the regex that's crafted for the main body of the bill.
    
    Added click to handle arg parsing, and flags for skipping checks and
    printing with more verbosity to diagnose check failures.
    
    The file path argument supports shell-expansion, such as `*`.

--- utility_bills_processor/national_grid_gas.py
+++ utility_bills_processor/national_grid_gas.py
@@ -3,11 +3,14 @@ import re
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta
 from pathlib import Path
-from typing import List, Optional, Union
+from typing import List, Optional, Sequence, Union
 
+import click
 from pypdf import PdfReader
 
 from ._common.dataclass_converters import ConversionDescriptor
+
+__VERBOSE = False
 
 __EXAMPLE = """
    CURRENT BILL ITEMIZED      SUMMARY OF CHARGES
@@ -40,9 +43,9 @@ __PATTERN = (
     r" (?P<current_method>[^\s]*)             (?P<current_meter_reading>\d+)"
     r".*(?P<previous_date>[a-zA-Z]{3} [0-9]{2} 20\d{2}) reading"
     r" (?P<previous_method>[^\s]*)"
-    r"                ____         (?P<previous_meter_reading>\d+)"
+    r" *_* *(?P<previous_meter_reading>\d+)"
     r".*Thermal Factor.*x(?P<thermal_factor>[\.0-9]+)"
-    r".*Total therms used.*(?P<total_therms>\d+)"
+    r".*Total therms used *(?P<total_therms>\d+)"
     r".*\$(?P<delivery_min_rate_usd>[\.0-9]+) per day for (?P=days_since_last_reading)"
     r" days"
     r".*First [\.0-9]+ therms @ \$(?P<delivery_first_tier_rate_usd>[\.0-9]+)"
@@ -53,7 +56,8 @@ __PATTERN = (
     r".*GAS SUPPLY CHARGE"
     r".*@ \$(?P<supply_rate>[\.0-9]+) /therm"
     r".*Paperless Bill Credit.*(?P<paperless_bill_credit_usd>-[\.0-9]+)"
-    r".*TOTAL CURRENT CHARGES.*\$(?P<total_usd>[\.0-9]+)"
+    r".*TOTAL CURRENT CHARGES *\$(?P<total_usd>[\.0-9]+)"
+    r".*(IMPORTANT MESSAGES)?.*(ADDITIONAL MESSAGES)?"
     r".*"
 )
 
@@ -121,9 +125,9 @@ class GasBill:
         calculated = self.current_date - self.previous_date
         if calculated != timedelta(days=self.days_since_last_reading):
             raise ValueError(
-                f"Dates don't match: current_date={self.current_date}"
-                + f" previous_date={self.previous_date} => calculated={calculated}"
-                + f" days_since_last_reading={self.days_since_last_reading}"
+                f"Dates don't match: {self.current_date=}"
+                + f" {self.previous_date=} => {calculated=}"
+                + f" {self.days_since_last_reading=}"
             )
 
         calculated = round(
@@ -134,10 +138,10 @@ class GasBill:
         if calculated != self.total_therms:
             raise ValueError(
                 "Readings and therms don't match:"
-                + f" current_meter_reading={self.current_meter_reading}"
-                + f" previous_meter_reading={self.previous_meter_reading}"
-                + f" thermal_factor={self.thermal_factor}"
-                + f" => calculated={calculated} != total_therms={self.total_therms}"
+                + f" {self.current_meter_reading=}"
+                + f" {self.previous_meter_reading=}"
+                + f" {self.thermal_factor=}"
+                + f" => {calculated=} != {self.total_therms=}"
             )
 
         calculated = round(
@@ -152,13 +156,12 @@ class GasBill:
         if calculated != self.delivery_total_usd:
             raise ValueError(
                 "Delivery rates and therms don't match total:"
-                + f" total_therms={self.total_therms}"
-                + f" delivery_min_rate_usd={self.delivery_min_rate_usd}"
-                + f" delivery_first_tier_rate_usd={self.delivery_first_tier_rate_usd}"
-                + " delivery_distribution_adjustment_rate="
-                + f"{self.delivery_distribution_adjustment_rate}"
-                + f" => calculated={calculated}"
-                + f" != delivery_total_usd={self.delivery_total_usd}"
+                + f" {self.total_therms=}"
+                + f" {self.delivery_min_rate_usd=}"
+                + f" {self.delivery_first_tier_rate_usd=}"
+                + f" {self.delivery_distribution_adjustment_rate=}"
+                + f" => {calculated=}"
+                + f" != {self.delivery_total_usd=}"
             )
 
         calculated = round(
@@ -169,10 +172,10 @@ class GasBill:
         )
         if calculated != self.total_usd:
             raise ValueError(
-                f"Totals don't match: delivery_total_usd={self.delivery_total_usd}"
-                + f" supply_total_usd={self.supply_total_usd}"
-                + f" paperless_bill_credit_usd={self.paperless_bill_credit_usd}"
-                + f" => calculated={calculated} != total_usd={self.total_usd}"
+                f"Totals don't match: {self.delivery_total_usd=}"
+                + f" {self.supply_total_usd=}"
+                + f" {self.paperless_bill_credit_usd=}"
+                + f" => {calculated=} != {self.total_usd=}"
             )
 
     def to_header(self) -> List[str]:
@@ -235,27 +238,56 @@ def extract_gas_fields(file: Path, password: Optional[str]) -> GasBill:
 
     text = "\n\n".join((page.extract_text() for page in reader.pages))
     match = re.search(__PATTERN, text, flags=re.DOTALL)
+    if __VERBOSE:
+        print(text)
+        print(f"{match.groupdict() if match else None}")
     if match is None:
         raise RuntimeError(f"Cannot parse bill. Found text:\n{text}")
     return GasBill(**match.groupdict())
 
 
-def main():
+@click.command()
+@click.option(
+    "-c/-i",
+    "--check/--ignore-checks",
+    default=True,
+    help="Control if extracted values are checked against each other.",
+)
+@click.option("-v", "--verbose", default=False, help="Increase verbosity of output.")
+@click.option(
+    "-p",
+    "--password",
+    prompt=True,
+    prompt_required=False,
+    hide_input=True,
+    help="Use if the file is encrypted.",
+)
+@click.argument(
+    "bill_files",
+    type=click.Path(
+        file_okay=True,
+        readable=True,
+        exists=True,
+        resolve_path=True,
+        path_type=Path,
+    ),
+    nargs=-1,
+)
+def main(bill_files: tuple[Path], password: str | None, check: bool, verbose: bool):
     """Start processing the provided gas bill PDF."""
-    import sys
-
-    arg_len = len(sys.argv)
-    if arg_len < 2:
-        print(f"Usage: {sys.argv[0]} BILL_FILE [PASSWORD]")
-        sys.exit(1)
-
-    bill = extract_gas_fields(
-        Path(sys.argv[1]),
-        sys.argv[2] if arg_len >= 3 else None,
-    )
-    bill.validate()
-    print(", ".join(map(str, bill.to_header())))
-    print(", ".join(map(str, bill.to_row())))
+    __VERBOSE = verbose
+    bills = []
+    for bill_file in bill_files:
+        bill = extract_gas_fields(bill_file, password)
+        if check:
+            bill.validate()
+        else:
+            click.secho("Skipping data checks", fg="yellow")
+        bills.append(bill)
+    click.echo("-" * 80)
+    print(", ".join(map(str, bills[0].to_header())))
+    for bill in bills:
+        print(",".join(map(str, bill.to_row())))
 
 
 if __name__ == "__main__":
