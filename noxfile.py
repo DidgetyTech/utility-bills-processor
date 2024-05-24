e5f5d6e6126e8346232810ec994ec720807dd9b7
DidgetyTech <32182119+DidgetyTech@users.noreply.github.com>
2024-05-23 22:01:47 -0400

    Add tests to nox sessions

--- noxfile.py
+++ noxfile.py
@@ -11,3 +11,11 @@ def lint(session):
     session.run("black", ".")
     session.run("flake8")
     session.run("mypy", "--config-file", "pyproject.toml")
+
+
+@nox.session(python=["3.12"])
+def test_locally(session):
+    """Run tests that don't need networked resources."""
+    session.install("poetry")
+    session.run("poetry", "install", "--with=dev")
+    session.run("python", "-m", "unittest")
