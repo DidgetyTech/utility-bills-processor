c5b9ac3532c144f2755ceaa810319662ecb149b6
DidgetyTech <32182119+DidgetyTech@users.noreply.github.com>
2024-07-29 16:15:52 -0400

    Add test coverage to nox runs
    Also update .gitignore to be more specific, sorted, and exclude
    .coverage file.

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,9 @@
-# noqa: D100
 from nox_poetry import session
 
 
 @session(python=["3.12"])
 def test_locally(session):
     """Run tests that don't need networked resources."""
-    session.install("pytest", ".")
-    session.run("python", "-m", "pytest")
+    args = session.posargs or ["--cov"]
+    session.run_always("poetry", "install", "--with", "dev", external=True)
+    session.run("python", "-m", "pytest", *args)
